ec4b0be4ae62d5039d9e1fb3af34e9db393574fe
themawaras <ferdinandthema@gmail.com>
2023-11-03 00:00:41 +0700

    init home page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,101 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+
+function Home() {
+  return (
+    <>
+      <main>
+        <div>
+          <h1>Experience the Future of Digital Payments with e-wallet</h1>
+          <div>
+            <div>
+              <img src="/img/login-side.png" alt="hero-img" />
+            </div>
+            <div>
+              <p>Simplify Your Life with Secure and Convenient Mobile Payments</p>
+              <div>
+                <button>
+                  <svg width="25" height="25" viewBox="0 0 25 25" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path
+                      d="M4.78559 4.09508C4.58568 4.29499 4.51904 4.62818 4.51904 5.02801V19.755C4.51904 20.1548 4.65232 20.488 4.85223 20.6879L4.91887 20.7545L13.182 12.4914V12.3582L4.78559 4.09508Z"
+                      fill="url(#paint0_linear_22_788)"
+                    />
+                    <path d="M15.8474 15.2902L13.1152 12.5581V12.3582L15.8474 9.62601L15.914 9.69264L19.1793 11.5585C20.1122 12.0916 20.1122 12.9579 19.1793 13.491L15.8474 15.2902Z" fill="url(#paint1_linear_22_788)" />
+                    <path d="M15.9142 15.2236L13.1154 12.4248L4.78564 20.7545C5.11883 21.0877 5.5853 21.0877 6.18504 20.8211L15.9142 15.2236Z" fill="url(#paint2_linear_22_788)" />
+                    <path d="M15.9142 9.62607L6.18504 4.09514C5.5853 3.76195 5.11883 3.82859 4.78564 4.16178L13.1154 12.4249L15.9142 9.62607Z" fill="url(#paint3_linear_22_788)" />
+                    <path opacity="0.2" d="M15.8475 15.1569L6.18504 20.6212C5.65194 20.9544 5.18547 20.8878 4.85228 20.6212L4.78564 20.6879L4.85228 20.7545C5.18547 21.0211 5.65194 21.0877 6.18504 20.7545L15.8475 15.1569Z" fill="black" />
+                    <path
+                      opacity="0.2"
+                      d="M4.78559 20.6212C4.58568 20.4213 4.51904 20.0881 4.51904 19.6883V19.7549C4.51904 20.1548 4.65232 20.488 4.85223 20.6879V20.6212H4.78559ZM19.1794 13.2911L15.8475 15.1569L15.9141 15.2236L19.1794 13.3577C19.6458 13.0912 19.8457 12.758 19.8457 12.4248C19.8457 12.758 19.5792 13.0245 19.1794 13.2911Z"
+                      fill="black"
+                    />
+                    <path
+                      opacity="0.2"
+                      d="M6.18499 4.16174L19.1794 11.5585C19.5792 11.8251 19.8457 12.0916 19.8457 12.4248C19.8457 12.0916 19.6458 11.7585 19.1794 11.4919L6.18499 4.0951C5.25206 3.562 4.51904 3.96183 4.51904 5.02803V5.09467C4.51904 4.0951 5.25206 3.62864 6.18499 4.16174Z"
+                      fill="white"
+                    />
+                    <defs>
+                      <linearGradient id="paint0_linear_22_788" x1="12.3823" y1="4.90139" x2="1.19848" y2="16.0845" gradientUnits="userSpaceOnUse">
+                        <stop stop-color="#00A0FF" />
+                        <stop offset="0.007" stop-color="#00A1FF" />
+                        <stop offset="0.26" stop-color="#00BEFF" />
+                        <stop offset="0.512" stop-color="#00D2FF" />
+                        <stop offset="0.76" stop-color="#00DFFF" />
+                        <stop offset="1" stop-color="#00E3FF" />
+                      </linearGradient>
+                      <linearGradient id="paint1_linear_22_788" x1="20.4014" y1="12.4255" x2="4.27707" y2="12.4255" gradientUnits="userSpaceOnUse">
+                        <stop stop-color="#FFE000" />
+                        <stop offset="0.409" stop-color="#FFBD00" />
+                        <stop offset="0.775" stop-color="#FFA500" />
+                        <stop offset="1" stop-color="#FF9C00" />
+                      </linearGradient>
+                      <linearGradient id="paint2_linear_22_788" x1="14.3995" y1="13.9548" x2="-0.765949" y2="29.1202" gradientUnits="userSpaceOnUse">
+                        <stop stop-color="#FF3A44" />
+                        <stop offset="1" stop-color="#C31162" />
+                      </linearGradient>
+                      <linearGradient id="paint3_linear_22_788" x1="2.71787" y1="-0.785412" x2="9.49027" y2="5.98699" gradientUnits="userSpaceOnUse">
+                        <stop stop-color="#32A071" />
+                        <stop offset="0.069" stop-color="#2DA771" />
+                        <stop offset="0.476" stop-color="#15CF74" />
+                        <stop offset="0.801" stop-color="#06E775" />
+                        <stop offset="1" stop-color="#00F076" />
+                      </linearGradient>
+                    </defs>
+                  </svg>
+                  <p>Play Store</p>
+                </button>
+                <button>
+                  <svg width="25" height="25" viewBox="0 0 25 25" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path
+                      d="M16.778 12.7964C16.7602 10.6721 18.5166 9.63956 18.5938 9.59209C17.5969 8.14422 16.0541 7.94246 15.5141 7.92466C14.2205 7.78818 12.9625 8.70201 12.3038 8.70201C11.6333 8.70201 10.6127 7.94246 9.52082 7.96027C8.11448 7.984 6.79122 8.79695 6.07322 10.0668C4.5838 12.6481 5.69345 16.4458 7.12352 18.5345C7.84152 19.5551 8.67227 20.7004 9.77005 20.6588C10.8382 20.6173 11.2417 19.9764 12.5293 19.9764C13.811 19.9764 14.1849 20.6588 15.3005 20.6351C16.4516 20.6173 17.1696 19.6085 17.8639 18.576C18.6887 17.4071 19.021 16.2499 19.0329 16.1906C19.0032 16.1787 16.8017 15.3361 16.778 12.7964Z"
+                      fill="white"
+                    />
+                    <path
+                      d="M14.6718 6.554C15.2474 5.83599 15.6391 4.8569 15.5323 3.86C14.7015 3.8956 13.6571 4.43559 13.0578 5.14173C12.5297 5.76479 12.055 6.77949 12.1737 7.73485C13.1112 7.80605 14.0725 7.26013 14.6718 6.554Z"
+                      fill="white"
+                    />
+                  </svg>
+
+                  <p>Apps Store</p>
+                </button>
+              </div>
+              <div>
+                <h1>4.6 M</h1>
+                <img src="" alt="user-profile-img" />
+              </div>
+              <div>
+                <p>Around the world, we already have over 4.6 happy user</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </main>
+    </>
+  );
+}
+
+export default Home;
