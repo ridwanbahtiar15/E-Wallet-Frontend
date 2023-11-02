9d80e306e76f150bd1cb2c55644bb658f0c839cf
F Thema W <102793804+themawaras@users.noreply.github.com>
2023-11-02 21:51:11 +0700

    Update Router.jsx

--- src/Router.jsx
+++ src/Router.jsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 
 import Dashboard from "./pages/Dashboard";
 import HistoryTransaction from "./pages/HistoryTransaction";
+import Register from "./pages/register";
 
 const router = createBrowserRouter([
   {
@@ -11,6 +12,11 @@ const router = createBrowserRouter([
   {
     path: "/history",
     element: <HistoryTransaction />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
+    // errorElement: "",
   },
 ]);
 
