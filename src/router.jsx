c39c4b13b531afe4e7ffa5c9a0f1bd36be869491
themawaras <ferdinandthema@gmail.com>
2023-11-02 21:42:57 +0700

    merge Router.jsx

--- src/router.jsx
+++ src/router.jsx
@@ -1,8 +1,18 @@
 import { createBrowserRouter } from "react-router-dom";
 
+import Dashboard from "./pages/Dashboard";
+import HistoryTransaction from "./pages/HistoryTransaction";
 import Register from "./pages/register";
 
 const router = createBrowserRouter([
+  {
+    path: "/dashboard",
+    element: <Dashboard />,
+  },
+  {
+    path: "/history",
+    element: <HistoryTransaction />,
+  },
   {
     path: "/register",
     element: <Register />,
