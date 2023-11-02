8c2007e88610902d34880a632b4be2821483a351
adisucipto98 <adii.adisucipto@gmail.com>
2023-11-03 04:38:03 +0700

    Menambahkan fontFamiliy

--- src/pages/ForgotPass.jsx
+++ src/pages/ForgotPass.jsx
@@ -29,7 +29,7 @@ function ForgotPass() {
                     <div className='flex items-center gap-[15px] mb-[25px]'>
 
                         <img src="img/wallet.png" alt="" className='w-8 h-8 object-cover' />
-                        <h1 className='font-nunito-sans text-[#2948FF] text-xl font-semibold'>E-Wallet</h1>
+                        <h1 className='font-nunito text-[#2948FF] text-xl font-semibold'>E-Wallet</h1>
 
                     </div>
 
