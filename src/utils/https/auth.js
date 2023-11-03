e77e24c3f30f3e1eff73959f0cee047e993c1ce9
inabaaaaa <nakbarsyarif@gmail.com>
2023-11-03 20:20:51 +0700

    redux, slice user, redux persist

--- src/utils/https/auth.js
+++ src/utils/https/auth.js
@@ -6,3 +6,16 @@ export const register = (body) => {
   const url = baseUrl + "/register";
   return axios.post(url, body);
 };
+
+export const axiosLogin = (body) => {
+  const url = baseUrl + "/login";
+  return axios.post(url, body);
+};
+
+export const axiosLogout = (jwt) => {
+  return axios.delete(baseUrl, {
+    headers: {
+      Authorization: "Bearer " + jwt,
+    },
+  });
+};
